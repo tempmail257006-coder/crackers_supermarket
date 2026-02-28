ec7b85bb7d03e8783f829d66f765c4fe102f34da
tempmail <tempmail257006@gmail.com>
2026-02-28 14:21:25 +0530

    Removed large files and added gitignore

--- static/js/firebase-init.js
+++ static/js/firebase-init.js
@@ -1,22 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration (Authentication only for now).
 const firebaseConfig = {
-  apiKey: "AIzaSyAfDI3KNcIQhcA5GRh7G-q33jLk25xhLbY",
-  authDomain: "crackers-supermarket.firebaseapp.com",
-  databaseURL: "https://crackers-supermarket-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "crackers-supermarket",
-  storageBucket: "crackers-supermarket.firebasestorage.app",
-  messagingSenderId: "413659405126",
-  appId: "1:413659405126:web:b8c9747937ad401dad6347",
-  measurementId: "G-KKDYE7T56W"
+    apiKey: "AIzaSyAfDI3KNcIQhcA5GRh7G-q33jLk25xhLbY",
+    authDomain: "crackers-supermarket.firebaseapp.com",
+    projectId: "crackers-supermarket",
+    storageBucket: "crackers-supermarket.firebasestorage.app",
+    messagingSenderId: "413659405126",
+    appId: "1:413659405126:web:cf9f6e6e0cf99fd4ad6347"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+window.firebaseApp = app;
+window.firebaseAuth = auth;
+
+export { app, auth };
